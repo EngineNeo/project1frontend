c8e304bbed9e4faee14f55cb45c2a68d052da3aa
EngineNeo <neomaralit@gmail.com>
2023-09-13 21:50:20 -0400

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react';
 
 function App() {
+  const [healthStatus, setHealthStatus] = useState(null);
+
+  const checkHealth = () => {
+    fetch('http://localhost:8000/health-check')
+      .then(response => response.json())
+      .then(data => setHealthStatus(data.status))
+      .catch(error => console.error('Health check failed:', error));
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <button onClick={checkHealth}>Check Health</button>
+      {healthStatus && <p>Health status: {healthStatus}</p>}
     </div>
   );
 }
